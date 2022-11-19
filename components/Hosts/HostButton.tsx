53b879aa3290332d7cabf1acadc7893a1c13dd89
gregfromstl <gregfromstl@gmail.com>
2022-11-19 11:19:59 -0600

    retro host button

--- components/Hosts/HostButton.tsx
+++ components/Hosts/HostButton.tsx
@@ -9,7 +9,7 @@ export default function HostButton() {
     <>
       <button
         type="button"
-        className="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-200 hover:bg-primary-300 focus:outline-none focus:ring-2 focus:ring-primary-200 focus:ring-offset-2"
+        className="inline-flex items-center px-4 py-2 text-sm font-medium text-white transition rounded-full bg-primary-200 hover:bg-primary-300 focus:outline-none "
       >
         <MicrophoneIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
         Host
