68ed08bedb98bb3fa4b3424cede56194703e2f8d
gregfromstl <gregfromstl@gmail.com>
2022-11-22 20:48:58 -0600

    remove discord tag link

--- components/SocialTags/DiscordTag.tsx
+++ components/SocialTags/DiscordTag.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 export default function DisordTag({ handle }: { handle: string }) {
   return (
-    <Link
-      className="flex items-center justify-center gap-1 py-1 text-sm text-gray-500"
-      href={`https://twitter.com/${handle}`}
-      target="_blank"
-    >
+    <div className="flex items-center justify-center gap-1 py-1 text-sm text-gray-500">
       <Image src="/assets/discord.png" alt="Discord" width={16} height={16} />
       <p>{handle}</p>
-    </Link>
+    </div>
   );
 }
