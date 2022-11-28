d80a32e3b99083acb359446b5ee065c5e9fb5a9b
gregfromstl <gregfromstl@gmail.com>
2022-11-28 14:58:35 -0600

    Clean user menu

--- components/Header/UserMenu.tsx
+++ components/Header/UserMenu.tsx
@@ -44,32 +44,6 @@ export default function UserMenu({ user }: { user: User }) {
           <Menu.Item>
             {({ active }) => (
               <a
-                href="#"
-                className={classNames(
-                  active ? "bg-tan-500" : "",
-                  "block px-4 py-2 text-sm text-gray-700"
-                )}
-              >
-                Your Profile
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={classNames(
-                  active ? "bg-tan-500" : "",
-                  "block px-4 py-2 text-sm text-gray-700"
-                )}
-              >
-                Settings
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
                 onClick={signout}
                 className={classNames(
                   active ? "bg-tan-500" : "",
