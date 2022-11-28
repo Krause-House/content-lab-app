2bc37928c566210813d89aaddb830370d2759c7e
gregfromstl <gregfromstl@gmail.com>
2022-11-28 15:50:12 -0600

    remove local host data

--- components/Hosts/HostListItem.tsx
+++ components/Hosts/HostListItem.tsx
@@ -25,38 +25,23 @@ export default function HostListItem({
   host: HostData;
   userId: string | null;
 }) {
-  const [localHostData, setLocalHostData] = useState(host);
   const [isWaiting, setIsWaiting] = useState(false);
 
   const vote = useCallback(
     async (vote: VOTE) => {
       setIsWaiting(true);
-      const originalHostData = localHostData;
       try {
         if (userId) {
-          if (vote === VOTE.FOR && localHostData.for.includes(userId)) {
+          if (vote === VOTE.FOR && host.for.includes(userId)) {
             throw new Error("User has already voted for this host");
           }
-          if (vote === VOTE.AGAINST && localHostData.against.includes(userId)) {
+          if (vote === VOTE.AGAINST && host.against.includes(userId)) {
             throw new Error("User has already voted against this host");
           }
-          setLocalHostData({
-            ...localHostData,
-            for:
-              vote === VOTE.FOR
-                ? [...localHostData.for, userId]
-                : localHostData.for.filter((id) => id !== userId),
-            against:
-              vote === VOTE.AGAINST
-                ? [...localHostData.against, userId]
-                : localHostData.against.filter((id) => id !== userId),
-          });
           const { data } = await act(host, userId, vote, router.refresh);
-          setLocalHostData(data); // ensure the local host data matches what's in the DB
         } else throw new Error("User is not logged in");
       } catch (e) {
         console.log(e);
-        setLocalHostData(originalHostData);
       } finally {
         setIsWaiting(false);
       }
@@ -83,36 +68,36 @@ export default function HostListItem({
           </div>
         </div>
         <div className="flex flex-col items-center flex-shrink-0 mx-2 text-sm font-medium text-gray-400">
-          {userId && (
-            <ChevronUpIcon
-              onClick={() =>
-                !localHostData.for.includes(userId) &&
-                !isWaiting &&
-                vote(VOTE.FOR)
-              }
-              className={`w-5 h-5 hover:text-gray-600 ${
-                !localHostData.for.includes(userId) && !isWaiting
-                  ? "cursor-pointer"
-                  : "text-gray-600"
-              }`}
-            />
-          )}
-          <p className="text-gray-600">
-            {localHostData.for.length - localHostData.against.length}
-          </p>
-          {userId && (
-            <ChevronDownIcon
-              onClick={() =>
-                !localHostData.against.includes(userId) &&
-                !isWaiting &&
-                vote(VOTE.AGAINST)
-              }
-              className={`w-5 h-5 hover:text-gray-600 ${
-                !localHostData.against.includes(userId) && !isWaiting
-                  ? "cursor-pointer"
-                  : "text-gray-600"
-              }`}
-            />
+          {isWaiting ? (
+            "Loading..."
+          ) : (
+            <>
+              {userId && (
+                <ChevronUpIcon
+                  onClick={() => !host.for.includes(userId) && vote(VOTE.FOR)}
+                  className={`w-5 h-5 hover:text-gray-600 ${
+                    !host.for.includes(userId)
+                      ? "cursor-pointer"
+                      : "text-gray-600"
+                  }`}
+                />
+              )}
+              <p className="text-gray-600">
+                {host.for.length - host.against.length}
+              </p>
+              {userId && (
+                <ChevronDownIcon
+                  onClick={() =>
+                    !host.against.includes(userId) && vote(VOTE.AGAINST)
+                  }
+                  className={`w-5 h-5 hover:text-gray-600 ${
+                    !host.against.includes(userId)
+                      ? "cursor-pointer"
+                      : "text-gray-600"
+                  }`}
+                />
+              )}
+            </>
           )}
         </div>
       </div>
