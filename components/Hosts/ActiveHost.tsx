77bc86db12e8411f8356b94a2822280d6b43da77
gregfromstl <gregfromstl@gmail.com>
2022-11-30 13:02:38 -0600

    Prep for non-live

--- components/Hosts/ActiveHost.tsx
+++ components/Hosts/ActiveHost.tsx
@@ -25,7 +25,13 @@ const watching = {
   ],
 };
 
-export default function ActiveHost({ hostsName }: { hostsName: string }) {
+export default function ActiveHost({
+  hostsName,
+  isLive = false,
+}: {
+  hostsName: string;
+  isLive?: boolean;
+}) {
   return (
     <Card className="my-8 bg-primary-200 card-shadow">
       <a
@@ -38,33 +44,39 @@ export default function ActiveHost({ hostsName }: { hostsName: string }) {
           <div className="flex-1 min-w-0 sm:flex sm:items-center sm:justify-between">
             <div className="">
               <label className="flex text-xs text-gray-300 sm:text-sm">
-                Currently Hosting
+                {isLive ? "Currently Hosting" : "Opening Hosts"}
               </label>
 
               <h3 className="truncate text-gray-50">{hostsName}</h3>
 
-              <div className="flex gap-2 mt-3 text-gray-300">
-                <div className="flex -space-x-1">
-                  {watching.users.map((user) => (
-                    <Image
-                      width={24}
-                      height={24}
-                      key={user.id}
-                      className="inline-block w-6 h-6 rounded-full ring-2 ring-gray-50"
-                      src={user.imageUrl}
-                      alt={user.name}
-                    />
-                  ))}
+              {isLive && (
+                <div className="flex gap-2 mt-3 text-gray-300">
+                  <div className="flex -space-x-1">
+                    {watching.users.map((user) => (
+                      <Image
+                        width={24}
+                        height={24}
+                        key={user.id}
+                        className="inline-block w-6 h-6 rounded-full ring-2 ring-gray-50"
+                        src={user.imageUrl}
+                        alt={user.name}
+                      />
+                    ))}
+                  </div>
+                  <p className="hidden sm:block">
+                    {watching.watching} fans watching
+                  </p>
                 </div>
-                <p className="hidden sm:block">
-                  {watching.watching} fans watching
-                </p>
-              </div>
+              )}
             </div>
           </div>
           <div className="flex items-center flex-shrink-0 gap-1 ml-5 font-medium text-gray-50">
-            <p className="hidden sm:block">Watch now</p>
-            <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+            <p className="hidden sm:block">
+              {isLive ? "Watch now" : "7 PM EST"}
+            </p>
+            {isLive && (
+              <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+            )}
           </div>
         </div>
       </a>
