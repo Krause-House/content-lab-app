f55c4e5aa1fa7642140cb9d9218fe3af07b9dc7d
gregfromstl <gregfromstl@gmail.com>
2022-12-11 10:02:16 -0600

    Add metadata to home page

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,25 @@
 export default function Head() {
   return (
     <>
-      <title>Gameday</title>
+      <title>Gameday by Krause House</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Krause House Gameday" />
+      <meta
+        name="description"
+        content="A decentralized media platform powered by the world's greatest basketball community. Tipping off this December."
+      />
+      <meta property="og:title" content="Gameday by Krause House" />
+      <meta
+        property="og:description"
+        content="A decentralized media platform powered by the world's greatest basketball community. Tipping off this December."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@KrauseHouseDAO" />
+      <meta name="twitter:creator" content="@gregfromstl" />
+      <meta property="og:url" content="https://gameday.krausehouse.club" />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/fSFHKFf/preview.png"
+      />
       <link rel="icon" href="/favicon.ico" />
       <link rel="icon" href="/favicon.png" />
     </>
