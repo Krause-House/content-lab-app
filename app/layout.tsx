c4e6c3a3841e74356489cf5f72e1713bde062aec
gregfromstl <gregfromstl@gmail.com>
2022-12-19 16:38:44 -0600

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "~/components/Header";
 import SupabaseListener from "~/components/supabase-listener";
 import ToastContainer from "~/components/ToastContainer";
 import createClient from "~/util/supabase-server";
+import Script from "next/script";
 import "./globals.css";
 
 // do not cache this layout due to the access token
@@ -23,6 +24,24 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+			page_path: window.location.pathname,
+		});
+		`,
+        }}
+      />
       <SupabaseListener accessToken={session?.access_token} />
       <body className="relative overflow-x-hidden font-light bg-tan-500">
         <ToastContainer />
