affc272109d9fd0cb150daa8298089544aceaa36
gregfromstl <gregfromstl@gmail.com>
2022-12-24 11:25:23 -0600

    move auth redirect to page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,20 @@
+import "server-only";
+import createClient from "~/util/supabase-server";
 import AuthForm from "~/components/AuthForm";
+import { redirect } from "next/navigation";
 
-export default function SignUp({
+export default async function SignUp({
   searchParams,
 }: {
   searchParams?: { creator?: string; referredBy?: string; redirectTo?: string };
 }) {
+  const supabase = createClient();
+  const user = (await supabase.auth.getUser()).data.user;
+
+  if (user) {
+    redirect(searchParams?.redirectTo || "/");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-8 p-8 py-32">
       <AuthForm
