f30badffdfe65910b9f384df98e85b569f037ad0
gregfromstl <gregfromstl@gmail.com>
2022-12-26 13:06:21 -0600

    add YNG banner to signup page

--- app/creator/[creator_id]/signup/page.tsx
+++ app/creator/[creator_id]/signup/page.tsx
@@ -2,6 +2,7 @@ import "server-only";
 import createClient from "~/util/supabase-server";
 import AuthForm from "~/components/AuthForm";
 import { redirect } from "next/navigation";
+import BannerImage from "~/components/BannerImage";
 
 export default async function SignUp({
   params,
@@ -18,13 +19,18 @@ export default async function SignUp({
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 p-8 py-32">
-      <AuthForm
-        onlyMode="Sign Up"
-        creatorIdToSubscribeTo={params?.creator_id}
-        referredByEmail={searchParams?.referredBy}
-        redirectTo={searchParams?.redirectTo}
-      />
-    </div>
+    <>
+      <div className="hidden sm:block">
+        <BannerImage imageUrl="/assets/dreamerz_banner.png" />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-8 p-8 py-32">
+        <AuthForm
+          onlyMode="Sign Up"
+          creatorIdToSubscribeTo={params?.creator_id}
+          referredByEmail={searchParams?.referredBy}
+          redirectTo={searchParams?.redirectTo}
+        />
+      </div>
+    </>
   );
 }
