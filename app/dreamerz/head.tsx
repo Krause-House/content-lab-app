f080f3ddc580b235ff839307b9293e64a157e37b
gregfromstl <gregfromstl@gmail.com>
2022-12-27 10:08:21 -0600

    Add YNG Dreamerz metadata

---
+++ app/dreamerz/head.tsx
@@ -0,0 +1,27 @@
+export default function Head() {
+  return (
+    <>
+      <title>YNG Dreamerz Watch Party on Gameday</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Vote on the games and entertainment for this week's YNG Dreamerz NBA Watch Party on Wednesday night."
+      />
+      <meta property="og:title" content="YNG Dreamerz Watch Party on Gameday" />
+      <meta
+        property="og:description"
+        content="Vote on the games and entertainment for this week's YNG Dreamerz NBA Watch Party on Wednesday night."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@KrauseHouseDAO" />
+      <meta name="twitter:creator" content="@gregfromstl" />
+      <meta property="og:url" content="https://gameday.watch/dreamerz" />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/f84mMwG/YNG-Dreamerz-Watch-Party.png"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
