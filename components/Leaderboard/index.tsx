b6a5f8d7609ace733788e03b6c592f97c0fdb86f
gregfromstl <gregfromstl@gmail.com>
2023-01-09 14:35:24 -0600

    fix(Leaderboard): missing useEffect dependency

--- components/Leaderboard/index.tsx
+++ components/Leaderboard/index.tsx
@@ -86,7 +86,7 @@ export default function Leaderboard({
     return () => {
       supabase.removeChannel(channel);
     };
-  }, [_candidates]);
+  }, [_candidates, contest.id]);
 
   return (
     <>
