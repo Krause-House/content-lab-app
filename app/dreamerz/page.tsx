98dc5cab3cdd23e11affe45f290154fdbd2662b6
gregfromstl <gregfromstl@gmail.com>
2023-01-21 11:18:10 -0600

    fix build error

--- app/dreamerz/page.tsx
+++ app/dreamerz/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default async function Dreamerz() {
   redirect("/creator/2");
+  return <></>;
 }
--- app/weekly/page.tsx
+++ app/weekly/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default async function Weekly() {
   redirect("/creator/3");
+  return <></>;
 }
