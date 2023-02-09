e593256950c4b203b6954144730dd879c1d6072d
gregfromstl <gregfromstl@gmail.com>
2023-02-09 14:34:15 +0000

    add metadata for resources page

---
+++ app/resources/head.tsx
@@ -0,0 +1,27 @@
+export default function Head() {
+  return (
+    <>
+      <title>Gameday Resources</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="The best resources in all of basketball, in one place for all the hoop fanatics."
+      />
+      <meta property="og:title" content="Gameday Resources" />
+      <meta
+        property="og:description"
+        content="The best resources in all of basketball, in one place for all the hoop fanatics."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@KrauseHouseDAO" />
+      <meta name="twitter:creator" content="@gregfromstl" />
+      <meta property="og:url" content="https://gameday.watch" />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/fSFHKFf/preview.png"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
