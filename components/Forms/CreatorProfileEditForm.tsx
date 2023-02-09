c77a22f81bb2cabb779d44f5d2e2db96a0fb0aa0
gregfromstl <gregfromstl@gmail.com>
2023-02-09 13:25:18 +0000

    feat(CreatorProfileEditForm): allows creators to set links

--- components/Forms/CreatorProfileEditForm.tsx
+++ components/Forms/CreatorProfileEditForm.tsx
@@ -16,6 +16,11 @@ export default function CreatorProfileEditForm({
   const router = useRouter();
   const [name, setName] = useState(creator.name);
   const [bio, setBio] = useState(creator.bio ?? "");
+  const [links, setLinks] = useState({
+    watch: creator.watch_url ?? "",
+    read: creator.read_url ?? "",
+    listen: creator.listen_url ?? "",
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -27,7 +32,13 @@ export default function CreatorProfileEditForm({
       try {
         const { error } = await supabase
           .from("creators")
-          .update({ name, bio })
+          .update({
+            name,
+            bio,
+            watch_url: links.watch,
+            read_url: links.read,
+            listen_url: links.listen,
+          })
           .eq("id", creator.id);
         if (error) {
           throw error;
@@ -44,28 +55,58 @@ export default function CreatorProfileEditForm({
   };
 
   return (
-    <form className="flex flex-col items-start gap-2" onSubmit={handleSubmit}>
-      <Input
-        value={name}
-        type="text"
-        placeholder="Name"
-        className="max-w-md"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextArea
-        value={bio}
-        type="text"
-        placeholder="Bio"
-        className="max-w-md"
-        onChange={(e) => setBio(e.target.value)}
-      />
-      <PrimaryButton
-        type="submit"
-        isLoading={isLoading}
-        loadingText="Saving..."
-      >
-        Save
-      </PrimaryButton>
+    <form
+      className="grid items-start grid-cols-1 gap-2 md:grid-cols-2"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col items-start gap-2">
+        <Input
+          value={name}
+          type="text"
+          placeholder="Name"
+          className="max-w-md"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextArea
+          value={bio}
+          type="text"
+          placeholder="Bio"
+          className="max-w-md"
+          onChange={(e) => setBio(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col items-start gap-2">
+        <Input
+          value={links.read}
+          type="text"
+          placeholder="Read Link"
+          className="max-w-md"
+          onChange={(e) => setLinks({ ...links, read: e.target.value })}
+        />
+        <Input
+          value={links.listen}
+          type="text"
+          placeholder="Listen Link"
+          className="max-w-md"
+          onChange={(e) => setLinks({ ...links, listen: e.target.value })}
+        />
+        <Input
+          value={links.watch}
+          type="text"
+          placeholder="Watch Link"
+          className="max-w-md"
+          onChange={(e) => setLinks({ ...links, watch: e.target.value })}
+        />
+      </div>
+      <div className="flex justify-start">
+        <PrimaryButton
+          type="submit"
+          isLoading={isLoading}
+          loadingText="Saving..."
+        >
+          Save
+        </PrimaryButton>
+      </div>
     </form>
   );
 }
