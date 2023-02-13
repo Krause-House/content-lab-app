9c9f146851394e066a75f237fc728535eedc1270
gregfromstl <gregfromstl@gmail.com>
2023-02-13 11:44:16 +0000

    fix card bg

--- app/page.tsx
+++ app/page.tsx
@@ -2,58 +2,139 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Card from "~/components/Card";
+import LeaderboardPreview from "~/components/Leaderboard/LeaderboardPreview";
+import Candidate from "~/types/Candidate";
+import Contest from "~/types/Contest";
 import Creator from "~/types/Creator";
 import createClient from "~/util/supabase-server";
 
 async function getCreators(): Promise<Creator[]> {
   const supabase = createClient();
-  const { data: creators } = await supabase
-    .from("creators")
-    .select()
-    .filter("is_visible", "eq", true);
+  const { data: creators } = await supabase.from("creators").select();
   if (!creators) {
     throw new Error("Failed to fetch creators");
   }
   return creators;
 }
 
+const getCandidatesInContest = (contest: Contest, candidates: Candidate[]) => {
+  // filter candidates by contest id
+  return candidates.filter((c) => c.contest_id === contest.id);
+};
+
+const countVotes = (contest: Contest, candidates: Candidate[]) => {
+  // get the candidates in the contest
+  const candidatesInContest = getCandidatesInContest(contest, candidates);
+  // get the total number of votes for the contest
+  const totalVotes = candidatesInContest.reduce(
+    (partialSum, c) => c.for.length + c.against.length + partialSum,
+    0
+  );
+  return totalVotes;
+};
+
+const getTrendingContests = async (
+  contests: Contest[],
+  candidates: Candidate[]
+) => {
+  // get the top contests based on number of votes
+  const topContests = contests
+    .sort((a, b) => countVotes(b, candidates) - countVotes(a, candidates))
+    .slice(0, 4);
+  return topContests;
+};
+
 export default async function Home() {
-  const creators = await getCreators();
+  const supabase = createClient();
+  const [
+    {
+      data: { user },
+    },
+    creators,
+    { data: contests },
+    { data: candidates },
+  ] = await Promise.all([
+    supabase.auth.getUser(),
+    getCreators(),
+    supabase.from("contests").select().eq("is_active", true),
+    supabase.from("candidates").select(),
+  ]);
+
+  const trendingContests =
+    contests && candidates
+      ? await getTrendingContests(contests, candidates)
+      : [];
+
   return (
     <main className="relative px-4 mx-auto max-w-7xl">
-      <div className="flex flex-col items-center justify-center gap-12 p-12 md:flex-row">
-        {creators.map((creator) => (
-          <Link
-            href={
-              (creator.page_url?.length ?? 0) > 0
-                ? creator.page_url!
-                : `/creator/${creator.id}`
-            }
-            key={creator.id}
-          >
-            <Card className="w-[351px] h-[453px] relative card-shadow-hover overflow-hidden cursor-pointer transition-shadow">
-              <div className="absolute inset-0 z-0 object-cover w-full h-full">
-                {creator.homepage_image_url && (
-                  <Image
-                    className="object-cover w-full h-full"
-                    src={creator.homepage_image_url}
-                    alt=""
-                    fill
+      <div className="py-12">
+        <div className="w-full pb-4">
+          <h1 className="text-gray-800">Featured Creators</h1>
+          <p className="mt-1 text-sm text-gray-500">
+            The best basketball creators on Gameday
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-12 md:flex-row">
+          {creators
+            .filter((c) => c.is_visible)
+            .map((creator) => (
+              <Link
+                href={
+                  (creator.page_url?.length ?? 0) > 0
+                    ? creator.page_url!
+                    : `/creator/${creator.id}`
+                }
+                key={creator.id}
+              >
+                <Card className="w-[345px] h-[445px] bg-primary relative card-shadow-hover overflow-hidden cursor-pointer transition-shadow">
+                  {creator.homepage_image_url && (
+                    <Image
+                      className="object-cover w-full h-full"
+                      src={creator.homepage_image_url}
+                      alt=""
+                      fill
+                    />
+                  )}
+                  <div
+                    className={`absolute w-full h-full bg-primary-500 ${
+                      creator.homepage_image_url && "opacity-40"
+                    }`}
                   />
-                )}
-                <div
-                  className={`w-full h-full bg-primary-500 ${
-                    creator.homepage_image_url && "opacity-40"
-                  }`}
-                />
-              </div>
-              <div className="absolute flex flex-col justify-between w-full h-full p-6">
-                <h1 className="text-white mega">{creator.name}</h1>
-                <p className="text-center text-white accent">Vote Now</p>
-              </div>
-            </Card>
-          </Link>
-        ))}
+                  <div className="absolute flex flex-col justify-between w-full h-full p-6">
+                    <h1 className="text-white mega">{creator.name}</h1>
+                    <p className="text-center text-white accent">Vote Now</p>
+                  </div>
+                </Card>
+              </Link>
+            ))}
+        </div>
+      </div>
+      <div className="py-12">
+        <div className="w-full pb-4">
+          <h1 className="text-gray-800">Trending Now</h1>
+          <p className="mt-1 text-sm text-gray-500">
+            The latest polls and contests across the Gameday ecosystem.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 gap-4 my-4 lg:grid-cols-2">
+          {candidates &&
+            trendingContests.map(
+              (contest) =>
+                creators.find((c) => c.id === contest.created_by) &&
+                candidates.filter((c) => c.contest_id === contest.id).length >
+                  0 && (
+                  <LeaderboardPreview
+                    user={user}
+                    creator={creators.find((c) => c.id === contest.created_by)!}
+                    contest={contest}
+                    candidates={candidates.filter(
+                      (c) => c.contest_id === contest.id
+                    )}
+                  />
+                )
+            )}
+        </div>
       </div>
     </main>
   );
---
+++ components/Leaderboard/LeaderboardPreview.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Card from "~/components/Card";
+import User from "~/types/User";
+import React from "react";
+import Contest, { CONTEST_TYPE } from "~/types/Contest";
+import Candidate from "~/types/Candidate";
+import LeaderboardListItem from "./LeaderboardListItem";
+import { PrimaryButton } from "~/components/Buttons";
+import Creator from "~/types/Creator";
+import Link from "next/link";
+
+export default function LeaderboardPreview({
+  user,
+  contest,
+  candidates,
+  creator,
+}: {
+  user: User | null;
+  contest: Contest;
+  candidates: Candidate[];
+  creator: Creator;
+}) {
+  const leadingCandidates = candidates
+    .sort(
+      (candidate1, candidate2) =>
+        candidate2.for.length -
+        candidate2.against.length -
+        (candidate1.for.length - candidate1.against.length)
+    )
+    .slice(0, 2);
+  return (
+    <>
+      <Card className="w-full bg-tan">
+        <div className="gap-2 px-4 py-5 border-b border-gray-300 md:flex md:items-center md:gap-4 md:justify-between sm:px-6">
+          <div className="w-full">
+            <h2 className="text-gray-800">
+              {creator.name} - {contest.name}
+            </h2>
+            <p className="mt-1 text-sm text-gray-500">{contest.description}</p>
+          </div>
+          <div className="flex items-end justify-end gap-2 mt-3 md:mt-0">
+            {contest.is_active &&
+              !user?.id &&
+              contest.type === CONTEST_TYPE.POLL && (
+                <Link href={`/creator/${creator.id}`}>
+                  <PrimaryButton>Vote</PrimaryButton>
+                </Link>
+              )}
+            {contest.is_active &&
+              user?.id &&
+              (contest.allow_submissions ?? false) && (
+                <Link href={`/creator/${creator.id}`}>
+                  <PrimaryButton>Submit</PrimaryButton>
+                </Link>
+              )}
+          </div>
+        </div>
+        <ul role="list" className="divide-y divide-gray-300">
+          {leadingCandidates.map((candidate) => (
+            <li key={candidate.id}>
+              <LeaderboardListItem
+                userEmail={user?.email ?? null}
+                canVote={!!user?.email && contest.is_active}
+                candidate={candidate}
+                vote={(_vote) => {}}
+                badges={candidate.is_winner ? ["Winner"] : []}
+                hasVoting={contest.type !== CONTEST_TYPE.SUBMISSIONS}
+              />
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </>
+  );
+}
