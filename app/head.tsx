aa252f1b319c24e2b20b72900c6c52f8579561ec
gregfromstl <gregfromstl@gmail.com>
2023-03-26 13:29:35 -0500

    dreamerz metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,24 +1,21 @@
 export default function Head() {
   return (
     <>
-      <title>Gameday by Krause House</title>
+      <title>YNG Dreamerz</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Join the world's greatest basketball community on Gameday."
+        content="Vote on your favorites from the YNG Dreamerz season"
       />
-      <meta property="og:title" content="Gameday by Krause House" />
+      <meta property="og:title" content="YNG Dreamerz" />
       <meta
         property="og:description"
-        content="Join the world's greatest basketball community on Gameday."
+        content="Vote on your favorites from the YNG Dreamerz season"
       />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@KrauseHouseDAO" />
-      <meta name="twitter:creator" content="@gregfromstl" />
-      <meta property="og:url" content="https://gameday.watch" />
       <meta
         property="og:image"
-        content="https://i.ibb.co/fSFHKFf/preview.png"
+        content="https://i.ibb.co/TWyXh8X/3p5k-L17l7z-XM9h-Tk.jpg"
       />
       <link rel="icon" href="/favicon.ico" />
       <link rel="icon" href="/favicon.png" />
